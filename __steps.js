bcdd8c60300df1ea55c65f7d8b190a9048f6e7d6
sowmik-sec <sowmik.sec@gmail.com>
2023-02-23 16:20:34 +0600

    initialize firebase auth and set sign in with google

--- __steps.js
+++ __steps.js
@@ -2,8 +2,13 @@
 1. go to console.firebase.google.com
 2. create a firebase project
 3. visit doc (go to docs) build > Authentication > web > get started.
-5. Register web app > firebase project home > click web > give name and register
-6. Install firebase for your project: npm install firebase
-7. DANGEROUS: get firebase config and put it in firebase.init.js
-8. export app from firebase.init.js 
+4. Register web app > firebase project home > click web > give name and register
+5. Install firebase for your project: npm install firebase
+6. DANGEROUS: get firebase config and put it in firebase.init.js
+7. export app from firebase.init.js 
+8. create auth using getAuth from firebase by using app from firebase.init.js 
+9. create a google auth provider. do not forget to use new GoogleAuthProvider()
+10. go to firebase > build > Authentication > Sign In method
+11. Enable google sign in method.
+12. Create a button for google sign in method with a click handler
 */
--- src/App.js
+++ src/App.js
@@ -1,23 +1,17 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import { getAuth, GoogleAuthProvider } from "firebase/auth";
+import { app } from "./firebase/firebase.init";
+
+const auth = getAuth(app);
 
 function App() {
+  const provider = new GoogleAuthProvider();
+  const handleGoogleSignIn = () => {
+    console.log("google authentication");
+  };
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <button onClick={handleGoogleSignIn}>Google Sign In</button>
     </div>
   );
 }
